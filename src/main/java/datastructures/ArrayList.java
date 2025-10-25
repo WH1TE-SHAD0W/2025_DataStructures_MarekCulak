963928302580f9a7be7cc8274c9cef7b282a4c69
Marek <marek@culak.sk>
2025-10-25 22:19:16 +0100

    chore(todo): created empty methods that are about to be integrated for write out as todo

--- src/main/java/datastructures/ArrayList.java
+++ src/main/java/datastructures/ArrayList.java
@@ -1,5 +1,61 @@
 package datastructures;
 
 public class ArrayList {
+    private String[] data;
+    private int size;
 
+    public ArrayList() {
+
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public String get(int index) {
+        return "";
+    }
+
+    public int indexOf(String s) {
+        return 0;
+    }
+
+    public void add(String element) {
+
+    }
+
+    public void add(String element, int index) {
+
+    }
+
+    public String set(String element, int index) {
+        String replaced_element = "";
+        return replaced_element;
+    }
+
+    public String remove(int index) {
+        String removed_element = "";
+        return removed_element;
+    }
+
+    public boolean removeAll(String element) {
+        return true;
+    }
+
+    public void clear() {
+
+    }
+
+    public ArrayList deduplicate() {
+        ArrayList deduplicated_list = new ArrayList();
+        return deduplicated_list;
+    }
+
+    public void rotate() {
+
+    }
+
+    public void join () {
+
+    }
 }
--- src/main/java/datastructures/LinkedList.java
+++ src/main/java/datastructures/LinkedList.java
@@ -1,5 +1,64 @@
 package datastructures;
 
 public class LinkedList {
+    public LinkedList() {
 
+    }
+
+    private static class Node {
+        public Node() {
+
+        }
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public String get(int index) {
+        return "";
+    }
+
+    public int indexOf(String s) {
+        return 0;
+    }
+
+    public void add(String element) {
+
+    }
+
+    public void add(String element, int index) {
+
+    }
+
+    public String set(String element, int index) {
+        String replaced_element = "";
+        return replaced_element;
+    }
+
+    public String remove(int index) {
+        String removed_element = "";
+        return removed_element;
+    }
+
+    public boolean removeAll(String element) {
+        return true;
+    }
+
+    public void clear() {
+
+    }
+
+    public LinkedList deduplicate() {
+        LinkedList deduplicated_list = new LinkedList();
+        return deduplicated_list;
+    }
+
+    public void rotate() {
+
+    }
+
+    public void join () {
+
+    }
 }
