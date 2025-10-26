2b1bf7cb7bf94ba3c2981870aa5775f7952cc1d8
Marek <marek@culak.sk>
2025-10-26 13:39:15 +0000

    feat: LinkedList class from in-class programming + add method

--- src/main/java/datastructures/LinkedList.java
+++ src/main/java/datastructures/LinkedList.java
@@ -1,18 +1,26 @@
 package datastructures;
 
 public class LinkedList {
-    public LinkedList() {
+    private int size;
+    private Node head;
+    private Node tail;
 
+    public LinkedList() {
+        this.size = 0;
     }
 
     private static class Node {
-        public Node() {
+        private Node next;
+        private int data;
 
+        public Node(int data) {
+            this.next = null;
+            this.data = data;
         }
     }
 
     public int size() {
-        return 0;
+        return this.size;
     }
 
     public String get(int index) {
@@ -23,12 +31,39 @@ public class LinkedList {
         return 0;
     }
 
-    public void add(String element) {
+    public void add(int value) {
+        Node newNode = new Node(value);
 
+        if (head == null) {
+            head = newNode;
+        }
+        else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
     }
 
-    public void add(String element, int index) {
+    public void add(int index, int value) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("");
+        }
 
+        Node newNode = new Node(value);
+        if (index == 0) {
+            newNode.next = head;
+            head = newNode;
+        } else {
+            Node current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+        }
+
+        size++;
     }
 
     public String set(String element, int index) {
